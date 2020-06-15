078dd9373c3c6623af00a7d21d27270dfd482ed1
Norc <64667579+Norc@users.noreply.github.com>
2020-06-15 00:30:42 -0400

    fixed overwriting except first

--- custom-hotbar.js
+++ custom-hotbar.js
@@ -192,7 +192,7 @@ class CustomHotbar extends Hotbar {
         icon: '<i class="fas fa-times"></i>',
         callback: li => {
 //          ui.CustomHotbar = this.assignCustomHotbarMacro(null, li.data("slot"));
-            this.assignCustomHotbarMacro(null, li.data("slot"));
+            ui.CustomHotbar.assignCustomHotbarMacro(null, li.data("slot"));
       }
       },
       {
@@ -249,7 +249,7 @@ class CustomHotbar extends Hotbar {
     // Only handle Macro drops
     const macro = await this._getDropMacro(data);
     if ( macro ) {
-      await this.assignCustomHotbarMacro(macro, li.dataset.slot, {fromSlot: data.slot});
+      await ui.CustomHotbar.assignCustomHotbarMacro(macro, li.dataset.slot, {fromSlot: data.slot});
       ui.CustomHotbar.render();
     }
   }
@@ -264,12 +264,11 @@ class CustomHotbar extends Hotbar {
   async _onClickMacro(event) {
     event.preventDefault();
     const li = event.currentTarget;
-    ui.CustomHotbar = this;
 
     // Case 1 - create a new Macro
     if ( li.classList.contains("inactive") ) {
       const macro = await Macro.create({name: "New Macro", type: "chat", scope: "global"});
-      ui.CustomHotbar = await this.assignCustomHotbarMacro(macro, li.dataset.slot);
+      await ui.CustomHotbar.assignCustomHotbarMacro(macro, li.dataset.slot);
       macro.sheet.render(true);
     }
 
@@ -419,9 +418,7 @@ Hooks.on("ready", async () => {
 customHotbarInit();
 console.log(chbMacroMap);
 if(!game.user.getFlag("custom-hotbar","chbMacroMap")) game.user.setFlag("custom-hotbar","chbMacroMap", chbMacroMap);
-ui.CustomHotbar.assignCustomHotbarMacro(game.macros.get("OywSAMO8HKVUNYK0"), 3);
-ui.CustomHotbar.assignCustomHotbarMacro(game.macros.get("tTk67L6Vo4fuahBm"), 2);
-ui.customHotbar.getCustomHotbarMacros(1);
+ui.CustomHotbar.getCustomHotbarMacros(1);
 ui.hotbar.render();
 });
 
