ccae0d1f676f06bbee81bb8970566b34036c7b31
Norc <64667579+Norc@users.noreply.github.com>
2020-07-08 13:10:16 -0400

    attempted to add a colorpicker setting.

--- custom-hotbar-settings.js
+++ custom-hotbar-settings.js
@@ -1,6 +1,7 @@
 export class CustomHotbarSettings {
 
     static register(){
+        //basic test
         game.settings.register("custom-hotbar", "chbColor", {
             name: "customHotbar.Settings.chbColor.value",
             hint: "customHotbar.Settings.chbColor.valueHint",
@@ -17,5 +18,19 @@ export class CustomHotbarSettings {
                 ui.customHotbar.render();
             }
         });
+        
+        To make a new color setting, make a new ColorSetting object:
+
+        //                                     module        key             options
+        new window.Ardittristan.ColorSetting("custom-hotbar", "chbCoreColor", {
+            name: "customHotbar.Settings.chbCoreColor.name",      // The name of the setting in the settings menu
+            hint: ""customHotbar.Settings.chbCoreColor.nameHint",   // A description of the registered setting and its behavior
+            label: "Color Picker",         // The text label used in the button
+            restricted: false,             // Restrict this setting to gamemaster only?
+            defaultColor: "#000000ff",     // The default color of the setting
+            scope: "client",               // The scope of the setting
+            onChange: (value) => {}        // A callback function which triggers when the setting is changed
+        })
+        
     }
 }
