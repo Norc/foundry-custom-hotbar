c9b100fa8ebc299eba825e2591f52ef6ba926894
Norc <64667579+Norc@users.noreply.github.com>
2020-07-09 04:15:48 -0400

    first working picker

--- color-setting.js
+++ color-setting.js
@@ -1,24 +1,3 @@
-export class CustomHotbarSettings {
-
-    static register(){
-        //basic test
-        game.settings.register("custom-hotbar", "chbColor", {
-            name: "customHotbar.Settings.chbColor.value",
-            hint: "customHotbar.Settings.chbColor.valueHint",
-            scope: "world",
-            config: true,
-            default: "#FFFFFF",
-            type: String,
-            /*choices: {
-            "default": "customHotbar.Settings.chbColor.value.default",
-            "onlyCurrent": "customHotbar.Settings.chbColor.value.onlyCurrent",
-            "no": "ForienQuestLog.Settings.showTasks.no"
-            },*/
-            onChange: value => {
-                ui.customHotbar.render();
-            }
-        });
-        
         //To make a new color setting, make a new ColorSetting object:
 
         //                                     module        key             options
@@ -31,7 +10,3 @@ export class CustomHotbarSettings {
             scope: "client",               // The scope of the setting
             onChange: (value) => {}        // A callback function which triggers when the setting is changed
         })
-
-
-    }
-}
