91f7fbc3ad6596dd4957d2db92f9daae1f75ae05
Norc <64667579+Norc@users.noreply.github.com>
2020-07-13 13:04:36 -0400

    syntax fixes from merge.

--- custom-hotbar-settings-form.js
+++ custom-hotbar-settings-form.js
@@ -49,21 +49,21 @@ export class CustomHotbarSettingsForm extends FormApplication {
      */
     //this is currently defined for an onload not a submit...
     async _updateObject(e, d) {
-        console.debug("Custom Hotbar | Attempting to update settings...");
-        d.chbPrimaryColor = game.settings.set{"custom-hotbar", "chbPrimaryColor");
-        d.chbBorderColor = game.settings.set{"custom-hotbar", "chbBorderColor");
-        d.chbBorderColorActive = game.settings.set{"custom-hotbar", "chbBorderColorActive");
-        d.chbBorderColorInactive = game.settings.set{"custom-hotbar", "chbBorderColorActive");
-        d.chbXPos = game.settings.set("custom-hotbar","chbXPos");
-        d.chbYPos = game.settings.set("custom-hotbar","chbYPos");                                                     
+        //console.debug("Custom Hotbar | Attempting to update settings...");
+        game.settings.set("custom-hotbar", "chbPrimaryColor", d.chbPrimaryColor);
+        game.settings.set("custom-hotbar", "chbBorderColor", d.chbBorderColor);
+        game.settings.set("custom-hotbar", "chbBorderColorActive", d.chbBorderColorActive);
+        game.settings.set("custom-hotbar", "chbBorderColorActive", d.hbBorderColorInactive);
+        game.settings.set("custom-hotbar","chbXPos", d.chbXPos);
+        game.settings.set("custom-hotbar","chbYPos", d.chbYPos);                                                     
     }
     
     onReset() {
         console.debug("Custom Hotbar | Attempting to reset chbSettingsForm to defaults");
-        d.chbPrimaryColor = game.settings.get{"custom-hotbar", "chbPrimaryColor.default");
-        d.chbBorderColor = game.settings.get{"custom-hotbar", "chbBorderColor.default");
-        d.chbBorderColorActive = game.settings.get{"custom-hotbar", "chbBorderColorActive.default");
-        d.chbBorderColorInactive = game.settings.get{"custom-hotbar", "chbBorderColorActive.default");
+        d.chbPrimaryColor = game.settings.get("custom-hotbar", "chbPrimaryColor.default");
+        d.chbBorderColor = game.settings.get("custom-hotbar", "chbBorderColor.default");
+        d.chbBorderColorActive = game.settings.get("custom-hotbar", "chbBorderColorActive.default");
+        d.chbBorderColorInactive = game.settings.get("custom-hotbar", "chbBorderColorActive.default");
         d.chbXPos = game.settings.get("custom-hotbar","chbXPos.default");
         d.chbYPos = game.settings.get("custom-hotbar","chbYPos.default");  
         this.render();
